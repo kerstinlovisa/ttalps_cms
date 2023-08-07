9eefd0f86f9d0941cdb8d83b474cd3b40064a658
jniedzie <jeremi.niedziela@cern.ch>
2023-08-07 14:12:10 +0200

    Improving errors handling. Making sure to reset also all elements of collections inside of an event.

--- core/include/Event.hpp
+++ core/include/Event.hpp
@@ -25,71 +25,81 @@ public:
 
   inline auto Get(std::string branchName)
   {
+    bool badBranch = false;
+
     if (values_types.count(branchName) == 0)
     {
-      std::cout << "\033[1;31m ERROR -- you're trying to access incorrect branch: " << branchName << "\033[0m"
+      std::cout << "\033[1;31m ERROR -- you're trying to access incorrect event-level branch: " << branchName << "\033[0m"
                 << std::endl;
+                badBranch = true;
     }
 
     struct result
     {
       operator UInt_t()
       {
+        if(badBranch) return 0;
         checkType("UInt_t");
         return event->GetUint(branchName);
       }
       operator Int_t()
       {
+        if(badBranch) return 0;
         checkType("Int_t");
         return event->GetInt(branchName);
       }
       operator Bool_t()
       {
+        if(badBranch) return 0;
         checkType("Bool_t");
         return event->GetBool(branchName);
       }
       operator Float_t()
       {
+        if(badBranch) return 0;
         checkType("Float_t");
         return event->GetFloat(branchName);
       }
       operator ULong64_t()
       {
+        if(badBranch) return 0;
         checkType("ULong64_t");
         return event->GetULong(branchName);
       }
       operator UChar_t()
       {
+        if(badBranch) return 0;
         checkType("UChar_t");
         return event->GetUChar(branchName);
       }
 
       Event *event;
       std::string branchName;
-
+      bool badBranch;
+      
       void checkType(std::string typeName)
       {
         std::string branchType = event->values_types[branchName];
         if (branchType != typeName)
         {
-          std::cout << "\033[1;33m WARNING -- you're trying to cast branch " << branchName << " (" << branchType << ") to " << typeName << "\033[0m"
+          std::cout << "\033[1;33m WARNING -- you're trying to cast an event-level branch \"" << branchName << "\" (" << branchType << ") to " << typeName << "\033[0m"
                     << std::endl;
         }
       }
     };
 
-    return result{this, branchName};
+    return result{this, branchName, badBranch};
   }
-
-  std::map<std::string, std::string> values_types; /// contains all branch names and corresponding types
 
 private:
   inline UInt_t GetUint(std::string branchName) { return values_uint[branchName]; }
-  inline UInt_t GetInt(std::string branchName) { return values_int[branchName]; }
+  inline Int_t GetInt(std::string branchName) { return values_int[branchName]; }
   inline Bool_t GetBool(std::string branchName) { return values_bool[branchName]; }
   inline Float_t GetFloat(std::string branchName) { return values_float[branchName]; }
   inline ULong64_t GetULong(std::string branchName) { return values_ulong[branchName]; }
   inline UChar_t GetUChar(std::string branchName) { return values_uchar[branchName]; }
+
+  std::map<std::string, std::string> values_types; /// contains all branch names and corresponding types
 
   std::map<std::string, UInt_t> values_uint;
   std::map<std::string, Int_t> values_int;
@@ -98,10 +108,10 @@ private:
   std::map<std::string, ULong64_t> values_ulong;
   std::map<std::string, UChar_t> values_uchar;
 
-  std::map<std::string, Float_t[maxCollectionElements]> values_float_vector;
-  std::map<std::string, UChar_t[maxCollectionElements]> values_uchar_vector;
   std::map<std::string, Int_t[maxCollectionElements]> values_int_vector;
   std::map<std::string, Bool_t[maxCollectionElements]> values_bool_vector;
+  std::map<std::string, Float_t[maxCollectionElements]> values_float_vector;
+  std::map<std::string, UChar_t[maxCollectionElements]> values_uchar_vector;
 
   std::map<std::string, std::shared_ptr<PhysicsObjects>> collections;
 
--- core/src/Event.cpp
+++ core/src/Event.cpp
@@ -14,14 +14,14 @@ Event::~Event()
 
 void Event::Reset()
 {
-  for (auto &[key, value] : values_uint)
-    value = 0;
-  for (auto &[key, value] : values_int)
-    value = 0;
-  for (auto &[key, value] : values_bool)
-    value = 0;
-  for (auto &[key, value] : values_float)
-    value = 0;
-  for (auto &[key, value] : values_ulong)
-    value = 0;
+  for (auto &[key, value] : values_uint) value = 0;
+  for (auto &[key, value] : values_int) value = 0;
+  for (auto &[key, value] : values_bool) value = 0;
+  for (auto &[key, value] : values_float) value = 0;
+  for (auto &[key, value] : values_ulong) value = 0;
+  for (auto &[key, value] : values_uchar) value = 0;
+
+  for(auto &[name, collection] : collections){
+    for(auto element : *collection) element->Reset();
+  }
 }
