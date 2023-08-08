7ddcda1d19fc4b48da7aabc068a02427da04fe86
jniedzie <jeremi.niedziela@cern.ch>
2023-08-08 08:03:24 +0200

    Reformatting to google style

--- core/include/EventWriter.hpp
+++ core/include/EventWriter.hpp
@@ -5,27 +5,26 @@
 #ifndef EventWriter_hpp
 #define EventWriter_hpp
 
-#include "Helpers.hpp"
 #include "Event.hpp"
 #include "EventReader.hpp"
+#include "Helpers.hpp"
 
-
-class EventWriter
-{
+class EventWriter {
 public:
-    EventWriter(std::string outputPath, const std::shared_ptr<EventReader> &eventReader_);
-    ~EventWriter();
+  EventWriter(std::string outputPath,
+              const std::shared_ptr<EventReader> &eventReader_);
+  ~EventWriter();
 
-    void AddCurrentEvent(std::string treeName);
-    void Save();
+  void AddCurrentEvent(std::string treeName);
+  void Save();
 
 private:
-    TFile *outFile;
-    std::map<std::string, TTree *> outputTrees;
+  TFile *outFile;
+  std::map<std::string, TTree *> outputTrees;
 
-    std::shared_ptr<EventReader> eventReader;
+  std::shared_ptr<EventReader> eventReader;
 
-    void SetupOutputTree(std::string outFileName);
+  void SetupOutputTree(std::string outFileName);
 };
 
 #endif /* EventWriter_hpp */
